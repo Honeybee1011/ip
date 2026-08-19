cc76ec1208f18e78d554677f00e8bb55f20d02a0
Honeybee101 <joshua.eu2@gmail.com>
2026-08-19 18:33:07 +0800

    Rename chatbot to Lloyd and add initial greeting flow

---
+++ src/main/java/Lloyd.java
@@ -0,0 +1,23 @@
+/**
+ * Starts the Lloyd chatbot application.
+ */
+public class Lloyd {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String banner = " _      _                 _\n"
+                + "| |    | |               | |\n"
+                + "| |    | | ___  _   _  __| |\n"
+                + "| |    | |/ _ \\| | | |/ _` |\n"
+                + "| |____| | (_) | |_| | (_| |\n"
+                + "|______|_|\\___/ \\__, |\\__,_|\n"
+                + "                 __/ |       \n"
+                + "                |___/        \n";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Lloyd (Water).");
+        System.out.println("What do you want?");
+        System.out.println(line);
+        System.out.println("Bye! Come back soon!");
+        System.out.println(line);
+    }
+}
