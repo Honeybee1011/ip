2d60e880d70507a8d825f909c090bfb165402ed9
Honeybee101 <joshua.eu2@gmail.com>
2026-08-19 18:42:54 +0800

    Edited banner whitespace and made intro line nicer.

--- src/main/java/Lloyd.java
+++ src/main/java/Lloyd.java
@@ -11,11 +11,11 @@ public class Lloyd {
                 + "| |____| | (_) | |_| | (_| |\n"
                 + "|______|_|\\___/ \\__, |\\__,_|\n"
                 + "                 __/ |       \n"
-                + "                |___/        \n";
+                + "                |___/        ";
         System.out.println(line);
         System.out.println(banner);
         System.out.println("Hello! I'm Lloyd (Water).");
-        System.out.println("What do you want?");
+        System.out.println("What can I get you?");
         System.out.println(line);
         System.out.println("Bye! Come back soon!");
         System.out.println(line);
