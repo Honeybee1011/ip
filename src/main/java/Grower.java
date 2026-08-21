029067a56495e2be2cd277c5d1e28c4fa918b04a
liownahum <e1528284@u.nus.edu>
2026-08-21 21:57:23 +0800

    Change file names and introductioni text to include new chat bot name Grow-er

---
+++ src/main/java/Grower.java
@@ -0,0 +1,8 @@
+public class Grower {
+    public static void main(String[] args) {
+        System.out.println("Goodday to you, I am Grow-er \n" +
+                "your accountability partner! \n" +
+                "i'm here to support your growth! \n" +
+                "What can I do for you today \n");
+    }
+}
